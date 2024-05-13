173988e09ff865ce070986f6c3f71477ba9b74c1
KanyaT <kanyathyagaraj@gmail.com>
2024-05-13 19:37:49 +0530

    lesson 04

--- app/src/main/java/com/example/testapk/MainActivity.kt
+++ app/src/main/java/com/example/testapk/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.testapk
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+
+        val imgLogo = findViewById<ImageView>(R.id.logo)
+        rounded.cornerRadius = 15f
+        imgLogo.setImageDrawable(rounded)
+
     }
 }
